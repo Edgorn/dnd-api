798011de69dac3eda15e47c46d8470ac68c000b4
Edgorn <edgarmaronda@gmail.com>
2024-04-19 17:08:27 +0200

    Mongo añadido

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect('mongodb://127.0.0.1:27017/dvd-5e')
+  .then(() => console.log('Conexión a MongoDB exitosa'))
+  .catch(err => console.error('Error al conectar a MongoDB', err));
+
+const razaSchema = new mongoose.Schema({
+  index: String,
+  name: String
+}, { collection: 'Raza' });
+
+const Raza = mongoose.model('Raza', razaSchema);
+
 app.get('/razas', async (req, res) => {
+
+  let razas2 = await Raza.find();
+  
+  console.log(razas2)
+
   const requestOptions = {
     headers: {
       'Content-Type': 'application/json'
@@ -16,8 +33,6 @@ app.get('/razas', async (req, res) => {
 
   const response = await axios.get("https://www.dnd5eapi.co/api/races", requestOptions)
   const razas = await Promise.all(response.data.results.map(async raza => await axios.get("https://www.dnd5eapi.co" + raza.url, requestOptions)))
-
-  // console.log(razas.map(r => r.data))
 
   const razasData = razas.map(r => r.data).map(r => {
     return {
