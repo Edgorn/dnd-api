b82841db09bb6f1830128041e406c34c3895a485
Edgorn <edgarmaronda@gmail.com>
2024-08-04 21:44:00 +0200

    Añadido console.error

--- src/domain/services/raza.service.js
+++ src/domain/services/raza.service.js
@@ -8,6 +8,7 @@ class RazaService {
       const razas = await this.razaRepository.obtenerTodas();
       return { success: true, data: razas };
     } catch (error) {
+      console.error(error)
       return { success: false, message: 'Error al recuperar las razas' };
     }
   }
